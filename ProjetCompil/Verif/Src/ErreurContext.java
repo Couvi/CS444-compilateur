0cee75f2795bb66f8f7afbd8de87a871ce8ccafc
bruchong <bruchong@esTPLINUXINFO.esisar.inpg.fr>
2016-10-05 14:20:20 +0200

    mise en place de la doc

--- ProjetCompil/Verif/Src/ErreurContext.java
+++ ProjetCompil/Verif/Src/ErreurContext.java
@@ -4,23 +4,65 @@
  * permet l'affichage des messages d'erreurs pour la passe 2.
  */
 
-// -------------------------------------------------------------------------
-// A COMPLETER, avec les différents types d'erreur et les messages d'erreurs 
-// correspondants
-// -------------------------------------------------------------------------
-
 package ProjetCompil.Verif.Src;
 
 public enum ErreurContext {
    
-   ErreurNonRepertoriee, TypeInconnu, TypesNonCompatible;
-
+   /**
+   * ErreurNonRepertoriee signifie qu'aucune autre erreur ne correspond au problème
+   * (cela ne devrait jamais ce produire ...)
+   */
+   ErreurNonRepertoriee,
+   
+   /**
+   * TypeInconnu est déclanché lorsque l'on cherche un type qui n'existe pas
+   */
+   TypeInconnu, 
+   
+   /**
+   * RedeclarationIdent est généré lorsque un identificateur à déjà été déclaré ou
+   * est réservé.
+   **/
+   RedeclarationIdent,
+   
+   /**
+   * IdentificateurInconnu est généré lorsque un identificateur n'a pas été déclaré préalablement
+   **/
+   IdentificateurInconnu,
+   
+   /**
+   * BorneNonEntier est généré lorsque l'on veut créer un type Interval mais les bornes ne sont pas des entiers
+   **/
+   BorneNonEntier,
+   
+   /**
+   * TypesNonCompatible signifie que l'opération n'est pas possible car les types des variables 
+   * dans l'opération ne sont pas compatible
+   */
+   TypesNonCompatible;
+   
+   /**
+   * Cette méthode permet de lever une exception en précisant l'erreur (type et ligne)
+   * 
+   * <b>Pour l'utiliser : </b> <br>
+   * ErreurContext err = ErreurContext.Nomdelerreur;
+   * err.leverErreurContext(String detail, int numLigne);
+   **/
    void leverErreurContext(String s, int numLigne) throws ErreurVerif {
       System.err.println("Erreur contextuelle : ");
       switch (this) {
-	      case TypeInconnu :
+	        case TypeInconnu :
 			System.err.println("Type inconnu ("+s+") ");
 			break;
+		case RedeclarationIdent :
+			System.out.println("L'identificateur "+s+" a déjà été déclaré ou est reservé ");
+			break;
+		case IdentificateurInconnu :
+			System.out.println("L'identificateur "+s+" n'a pas été déclaré et est inconnu ");
+			break;		
+		case BorneNonEntier :
+			System.out.println("Les bornes de l'Interval ne sont pas des entier ");
+			break;	
 		case TypesNonCompatible :
 			System.out.println("Types non compatible ( "+s+") ");
 			break;
@@ -33,8 +75,3 @@ public enum ErreurContext {
    }
 
 }
-
-
-//*** Pour utiliser tout ce merdier !!! ****/
-// ErreurContext err = ErreurContext.Erreurdemerde;
-// err.leverErreurContext(String s, int numLigne)
