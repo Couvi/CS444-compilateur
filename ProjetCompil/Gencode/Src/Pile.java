01225624884ea6068882897315104b09cb5b8485
root <root@localhost.localdomain>
2016-11-19 16:01:20 +0000

    prise en charge du noeud

--- ProjetCompil/Gencode/Src/Pile.java
+++ ProjetCompil/Gencode/Src/Pile.java
@@ -6,52 +6,49 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Pile {
-
-	private static int sommet = 1; //Premier emplacement vide par rapport à LB
-	private static int LB;
+	//Tout est exprimé par rapport à LB (qui se trouve entre la zone TEXT et la pile
+	private static int SP = 0;
+	private static int GB = 0;
 	private static boolean declaration = true;
-	private static Map<String, Tuple<Integer, Type> >variables = new HashMap<String, Tuple<Integer, Type> >();
 
-	public static void addGlobale(String nom, Type type) {
+	public static void addGlobale(Arbre a) {
 		if (declaration) {
-			variables.put(nom, new Tuple(sommet, type));
-			switch(type.getNature()){
-				case Boolean:
+			switch(a.getNoeud()){
+				/*case Boolean:
 				case Real:
 				case Interval:
-					sommet++;
+					SP++; GB++;
+					//Ajouter l'information dans le décor (SP)
 					break;
 				case Array: 
 					//todo !					
-					break;
+					break;*/
 			}
 		}
 	}
 
-	public static Tuple<Integer,Type> getGlobale(String nom) {
-		return variables.get(nom);
-	}
-
 	public static void finDeclaration() {
 		declaration = false;
-		LB = sommet-1;
-		Prog.ajouter(Inst.creation2(Operation.ADD, Operande.creationOpEntier(LB), Operande.opDirect(Registre.LB)));
-		//Ajouter déplacement de SP
+		Prog.ajouter(Inst.creation2(Operation.ADD, Operande.creationOpEntier(GB), Operande.opDirect(Registre.GB)));
+		Prog.ajouter(Inst.creation1(Operation.ADDSP, Operande.creationOpEntier(SP)));
+		SP = 0;
 	}
 
 
 	public static int allouer() {
-		//Augmenter SP
-		sommet++;
-		return sommet-LB;
+		if(!declaration) {
+			SP++;
+			Prog.ajouter(Inst.creation1(Operation.ADDSP, Operande.creationOpEntier(1)));
+			return SP;
+		}
+		return -1;
 	}
 
 	public static void liberer(int ref) {
-		if (ref == sommet) {
-			sommet--;
-			//Diminuer SP
+		if (!declaration && ref == SP) {
+			SP--;
+			Prog.ajouter(Inst.creation1(Operation.ADDSP, Operande.creationOpEntier(-1)));
 		}
-
-	  }
+	}
 	  
 }
