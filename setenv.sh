ec6db50e374e0b37097ab8cd1ea2dcb4805d0251
Alexis Bertrand <zipper97412@gmail.com>
2016-11-20 21:35:05 +0100

    ajout de la vm ima et des libs et maj de setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -2,3 +2,6 @@
 
 ABS_ROOT_DIR=`pwd`
 export CLASSPATH=$ABS_ROOT_DIR:$ABS_ROOT_DIR/ProjetCompil/Global/Bin/java-cup-11a-runtime.jar:$ABS_ROOT_DIR/ProjetCompil/Global/Bin/JFlex.jar:.
+export PATH=$PATH:$ABS_ROOT_DIR/ProjetCompil/Bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ABS_ROOT_DIR/ProjetCompil/Bin/libs-ima
+
